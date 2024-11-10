7e41761ecd2d341ee84c4f3be2ffa14f70abca97
SAHEED-KESLI <kasalisaheed2017@gmail.com>
2024-11-10 10:05:32 +0100

    second commit1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Waiting for Prisma to start..."
 ./wait-for db:5432 
 
-echo "Migrating the databse..."
-npm run prisma:dev:deploy 
+# echo "Migrating the databse..."
+# npm run prisma:dev:deploy 
 
 echo "Starting the server..."
 npm start 
